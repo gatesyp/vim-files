521d093057abe2fc9101150bb068d7f5629a5f8e
Steven Gates <ig11@zips.uakron.edu>
2017-02-18 02:54:39 -0500

    refreshed setup script

---
+++ setup-vim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# sets up vim and configures it 
+clear
+
+echo "Setting up vim"
+
+sudo apt-get install vim
+cp -r .vim ~/
+cp .vimrc ~/
+# copy the file which will update github to reflect changes
+cp update-vim-repo.sh ~/
+
+# install package manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "installing plugins"
+
+vim +PluginInstall +qall
+
+echo "all finished"
