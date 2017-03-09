0f1d34b1ed5e1a163d2c98484c626373dfbb1553
gatesyp <ig11@zips.uakron.edu>
2017-03-09 09:23:01 -0500

    Adjusted setup scripts.

--- push-vim-upstream.sh
+++ push-vim-upstream.sh
@@ -1,13 +1,13 @@
 # Script to auto update git repo when vimrc is changed, or plugins are installed. 
 
 # Copy the updated files back to local repo. 
-cp ~/.vimrc ~/GitHub/vim-files
-cp  ~/.vim/plugins.vim ~/GitHub/vim-files/.vim
+cp ~/.vimrc ~/github/vim-files
+cp  ~/.vim/plugins.vim ~/github/vim-files/.vim
 
 echo "Enter commit message: "
 read commit_msg
 
 # Commit and push my changes. 
-cd ~/GitHub/vim-files
+cd ~/github/vim-files
 git commit . -m "$commit_msg"
 git push
--- setup-vim.sh
+++ setup-vim.sh
@@ -5,12 +5,12 @@ clear
 
 echo "Setting up vim. "
 
-sudo apt-get install vim
+# sudo apt-get install vim
 cp -r .vim ~/
 cp .vimrc ~/
 
 # Copy the file which will update github to reflect changes. 
-cp update-vim-repo.sh ~/
+cp push-vim-upsteam.sh ~/
 
 # Install package manager. 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
